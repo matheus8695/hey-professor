d2b94d7cff8f86df786605b741fb3dd15fe6dc6d
matheus8695 <0821matheus@gmail.com>
2023-12-27 10:51:35 -0300

    hey-04: Handling N+1 issue

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -12,7 +12,7 @@
             <button class="flex items-start space-x-2 text-green-500">
                 <x-icons.thumbs-up class="w-5 h-5 hover:text-green-300 cursor-pointer" id="thumbs-up"/>
                 {{-- propriedade computada, acessada na App\Models\Question --}}
-                <span>{{ $question->likes }}</span>
+                <span>{{ $question->votes_sum_like ?: 0 }}</span>
             </button>
         </x-form>
 
@@ -20,7 +20,7 @@
             <button class="flex items-start space-x-2 text-red-500">
                 <x-icons.thumbs-down class="w-5 h-5 hover:text-red-300 cursor-pointer" id="thumbs-up"/>
                 {{-- propriedade computada, acessada na App\Models\Question --}}
-                <span>{{ $question->unlikes }}</span>
+                <span>{{ $question->votes_sum_unlike ?: 0 }}</span>
             </button>
         </x-form>
     </div>
