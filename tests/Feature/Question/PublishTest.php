ab391622668b7b23ab131c50d2df6ad857931f37
matheus8695 <0821matheus@gmail.com>
2023-12-27 16:35:08 -0300

    hey-05: It should be able to publish a question

--- tests/Feature/Question/PublishTest.php
+++ tests/Feature/Question/PublishTest.php
@@ -6,7 +6,7 @@ use function Pest\Laravel\{actingAs, put};
 
 it('should be able to publish a question', function () {
     $user     = User::factory()->create();
-    $question = Question::factory()->create();
+    $question = Question::factory()->create(['draft' => true]);
 
     actingAs($user);
 
