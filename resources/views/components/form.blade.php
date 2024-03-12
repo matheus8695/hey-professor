817680c90b8d4cc9157d5b569c84cc40cce87014
matheus8695 <0821matheus@gmail.com>
2024-03-12 15:19:08 -0300

    hey-08: implementing the frontend for the archive/restore options

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -2,13 +2,19 @@
     'action',
     'post' => null,
     'put' => null,
+    'patch' => null,
     'delete' => null,
 ])
 
 <form action="{{ $action }}" method="post" class="mx-auto">
     @csrf
+
     @if($put)
         @method('PUT')
+    @endif
+
+    @if($patch)
+        @method('PATCH')
     @endif
 
     @if($delete)
